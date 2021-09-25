cd173e421b1526df5cad012d21d25bc68a85857b
Uktam Hudoyarov <uktamyb@gmail.com>
2021-09-25 22:28:40 +0500

    mohirdev_test

---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container, Wrapper, BG } from "./style";
+
+function App() {
+  return (
+    <Container className="container">
+      <BG />
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/components/App/style.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import BG from "../../img/top-background.svg";
+
+export const Container = styled.div`
+  border: 1px solid green;
+  width: 1440px;
+  height: 7354px;
+`;
+
+export const BG = styled.img`
+  width: 4019px;
+  height: 3203px;
+`;
