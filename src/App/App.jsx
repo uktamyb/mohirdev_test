e92098f0c80bf8b313c15918f35ff0426359f10e
Uktam Hudoyarov <uktamyb@gmail.com>
2021-09-28 10:15:31 +0500

    changes

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect
-} from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import { Container } from "./style";
 
 import Header from "../components/Header";
